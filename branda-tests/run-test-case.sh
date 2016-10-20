81efc19b292a87c74194e01b0be7cbde3b70d43f
Hendrik Maus <aidentailor@gmail.com>
2016-10-20 08:43:21 +0200

    Add an exception scenario to he branda tests

--- branda-tests/run-test-case.sh
+++ branda-tests/run-test-case.sh
@@ -4,4 +4,5 @@ bin/spas run \
     --file "branda-tests/spas-input.apib.refract.json" \
     --type apib-refract \
     --base_uri http://localhost:8000 \
-    --request_provider "\Hmaus\Spas\Parser\Apib\ApibParsedRequestsProvider"
+    --request_provider "\Hmaus\Spas\Parser\Apib\ApibParsedRequestsProvider" \
+    --full_output
