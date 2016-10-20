e01aa0aa9d6d3ef1adedb91477e4a469c8cbfa76
Hendrik Maus <aidentailor@gmail.com>
2016-10-20 12:48:33 +0200

    No full output in example branda runs

--- branda-tests/run-test-case.sh
+++ branda-tests/run-test-case.sh
@@ -4,5 +4,4 @@ bin/spas run \
     --file "branda-tests/spas-input.apib.refract.json" \
     --type apib-refract \
     --base_uri http://localhost:8000 \
-    --request_provider "\Hmaus\Spas\Parser\Apib\ApibParsedRequestsProvider" \
-    --full_output
+    --request_provider "\Hmaus\Spas\Parser\Apib\ApibParsedRequestsProvider"
